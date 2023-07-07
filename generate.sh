ee5061a6edfa415070e6e2e70a28736a9c3d3d07
Gabriel Bustillo del Cuvillo <gabibust@gmail.com>
2023-07-07 19:10:51 +0200

    Initial Commit (since this fork)

---
+++ generate.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+NAME="BustilloDelCuvilloGabriel"
+
+NAMEFILE="minimal-latex-cv";
+LANGS="es en";
+JOBTYPES="dev gamedev";
+
+getResumeLocalized()
+{
+
+case $1 in
+    "en")
+        echo "resume"
+        ;;
+    "es")
+        echo "cv"
+        ;;
+esac
+echo "";
+}
+
+if [ ! -d $PWD/output ];then
+mkdir $PWD/output
+fi
+
+for JOBTYPE in $JOBTYPES
+do
+    for LANG in $LANGS
+    do
+        pdflatex -synctex=1 -interaction=nonstopmode '\def \langsa {'$LANG'} \def \jobtype {'$JOBTYPE'} \input{'$NAMEFILE'.tex}'
+        mv $NAMEFILE.pdf $PWD/output/`getResumeLocalized $LANG`-$NAME-$LANG-$JOBTYPE.pdf
+    done
+done
+
+rm *.aux *.log *.out *.gz
